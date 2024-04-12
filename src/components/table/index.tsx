94fd497ec38e8338df74110b3cf80b1822e00686
Christoph Weissteiner <30799919+theaussie86@users.noreply.github.com>
2024-04-12 09:14:32 +0200

    add interactivity to table

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,115 +1,90 @@
 import TablePagination from "./pagination";
+import mData from "./MOCK_DATA.json";
+import { useMemo } from "react";
+import {
+  useReactTable,
+  getCoreRowModel,
+  getPaginationRowModel,
+  flexRender,
+  createColumnHelper,
+} from "@tanstack/react-table";
 
 function WeissteinerTable() {
-  const people = [
-    {
-      name: "Lindsay Walton",
-      title: "Front-end Developer",
-      email: "lindsay.walton@example.com",
-      role: "Member",
-    },
-    {
-      name: "John Doe",
-      title: "Software Engineer",
-      email: "john.doe@example.com",
-      role: "Member",
-    },
-    {
-      name: "Jane Smith",
-      title: "Product Manager",
-      email: "jane.smith@example.com",
-      role: "Member",
-    },
-    {
-      name: "Michael Johnson",
-      title: "UI Designer",
-      email: "michael.johnson@example.com",
-      role: "Member",
-    },
-    {
-      name: "Emily Davis",
-      title: "QA Engineer",
-      email: "emily.davis@example.com",
-      role: "Member",
-    },
-    {
-      name: "David Wilson",
-      title: "Backend Developer",
-      email: "david.wilson@example.com",
-      role: "Member",
-    },
-    {
-      name: "Sarah Thompson",
-      title: "Data Analyst",
-      email: "sarah.thompson@example.com",
-      role: "Member",
-    },
-    {
-      name: "Robert Brown",
-      title: "Project Manager",
-      email: "robert.brown@example.com",
-      role: "Member",
-    },
+  const data = useMemo(() => mData, []);
+
+  const columnHelper = createColumnHelper<(typeof data)[0]>();
+
+  const columns = [
+    columnHelper.accessor("created", {
+      cell: (cell) =>
+        new Intl.DateTimeFormat("de-DE", {
+          dateStyle: "medium",
+          timeZone: "Europe/Berlin",
+        }).format(new Date(cell.getValue())),
+      header: "Datum",
+    }),
+    columnHelper.accessor("description", {
+      cell: (cell) => cell.getValue(),
+      header: "Beschreibung",
+    }),
+    columnHelper.accessor("amount", {
+      cell: (cell) =>
+        (cell.getValue() / 100).toLocaleString("de-DE", {
+          style: "currency",
+          currency: "EUR",
+        }),
+      header: "Betrag",
+    }),
+    columnHelper.accessor("account", {
+      cell: (cell) => cell.getValue(),
+      header: "Konto",
+    }),
   ];
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  });
+
   return (
     <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
       <table className="min-w-full divide-y divide-gray-300">
         <thead className="bg-gray-50">
-          <tr>
-            <th
-              scope="col"
-              className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-            >
-              Name
-            </th>
-            <th
-              scope="col"
-              className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-            >
-              Title
-            </th>
-            <th
-              scope="col"
-              className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-            >
-              Email
-            </th>
-            <th
-              scope="col"
-              className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-            >
-              Role
-            </th>
-            <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
-              <span className="sr-only">Edit</span>
-            </th>
-          </tr>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((column) => (
+                <th
+                  key={column.id}
+                  scope="col"
+                  className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                >
+                  {flexRender(
+                    column.column.columnDef.header,
+                    column.getContext()
+                  )}
+                </th>
+              ))}
+            </tr>
+          ))}
         </thead>
         <tbody className="divide-y divide-gray-200 bg-white">
-          {people.map((person) => (
-            <tr key={person.email}>
-              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                {person.name}
-              </td>
-              <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                {person.title}
-              </td>
-              <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                {person.email}
-              </td>
-              <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                {person.role}
-              </td>
-              <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                  Edit<span className="sr-only">, {person.name}</span>
-                </a>
-              </td>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td
+                  key={cell.id}
+                  className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6"
+                >
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
             </tr>
           ))}
         </tbody>
       </table>
-      <TablePagination />
+      <TablePagination table={table} />
     </div>
   );
 }
