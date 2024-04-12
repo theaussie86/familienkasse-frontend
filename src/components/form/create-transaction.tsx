a2c297782c242a76c6967010db9a23aeb783d23d
Christoph Weissteiner <30799919+theaussie86@users.noreply.github.com>
2024-04-12 16:21:54 +0200

    added date picker

--- src/components/form/create-transaction.tsx
+++ src/components/form/create-transaction.tsx
@@ -13,6 +13,11 @@ import {
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { CalendarIcon } from "@heroicons/react/24/outline";
+import { format } from "date-fns";
+import { cn } from "../../util";
+import { Calendar } from "../ui/calendar";
 
 function CreateTransactionForm() {
   const form = useForm<CreateTransactionSchema>({
@@ -74,6 +79,43 @@ function CreateTransactionForm() {
                 </RadioGroup>
               </FormControl>
 
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="created"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>Datum</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant="outline"
+                      className={cn(
+                        "w-[240px] pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Datum auswählen</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                  />
+                </PopoverContent>
+              </Popover>
               <FormMessage />
             </FormItem>
           )}
